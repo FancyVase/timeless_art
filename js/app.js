2a6b4d8c694284c15b62b1b9d519d79c3a89e858
Hiram <mm.hiram@gmail.com>
2015-12-08 15:24:04 -0500

    Change font to Avenir, edit style, add smooth scrolling from info to day

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var images = new Array("01_surrealism", 
-	                   "02_impressionism",
-	                   "03_fauvism",
-	                   "04_art_nouveau",
-	                   "05_cubism",
-	                   "06_pop_art",
-	                   "07_photorealism");
+					   "02_impressionism",
+					   "03_fauvism",
+					   "04_art_nouveau",
+					   "05_cubism",
+					   "06_pop_art",
+					   "07_photorealism");
 
 var index = Math.floor(Math.random() * images.length);
 
@@ -12,9 +12,15 @@ function changeBg() {
 	var img = images[index];
 
 	$('#day').css('background-image', 'url('+'../timeless_art/img/' + img + '_day.jpg)' );
-	$('#night').css('background-image', 'url('+'../timeless_art/img/' + img + '_night.jpg)' );
+	$('#night').css('background-image', 'url('+'../timeless_art/img/' + img + '_day.jpg)' );
 }
 
 $(document).ready(function() {
 	changeBg();
+
+	$("#title").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#day").offset().top
+		}, 500);
+	});
 });
