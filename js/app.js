0c24a9d2d79d4c1acabe61851b72270badacefc8
Hiram <mm.hiram@gmail.com>
2015-12-10 13:03:54 -0500

    Final version, added New Image and Demo buttons, among other things

--- js/app.js
+++ js/app.js
@@ -8,15 +8,30 @@ var images = new Array("01_impressionism",
 					   "08_dadaism",
 					   "09_surrealism",
 					   "10_bauhaus",
-					   "11_abstract_impressionism",
+					   "11_abstract_expressionism",
 					   "12_pop_art",
 					   "13_photorealism");
 
+var titles = new Array("Claude Monet - Impression, Sunrise",
+	                   "Andre Derain - Landscape near Chatou",
+	                   "Scott Christian Sava - Art Nouveau 17",
+	                   "Pablo Picasso - Factory, Horta de Ebbo",
+	                   "Rodchenko, Stepanova - Books!",
+	                   "Edvard Munch - The Scream of Nature",
+	                   "Tullio Crali - Bombardamento Aereo",
+	                   "René Magritte - The Treachery of Images",
+	                   "Salvador Dalí - The Persistence of Memory",
+	                   "Wassily Kandinsky - Composition VIII, 1923",
+	                   "Jackson Pollock - Convergence",
+	                   "Roy Lichtenstein - Whaam!",
+	                   "Robert Neffson - Notre Dame De Paris");
+
 var index = Math.floor(Math.random() * images.length);
 
-function changeBg() {
+function updateImage() {
 	$('#day').css('background-image', 'url('+'../timeless_art/img/' + images[index] + '_day.jpg)' );
 	$('#night').css('background-image', 'url('+'../timeless_art/img/' + images[index] + '_night.jpg)' );
+	$('#name').text(titles[index]);
 }
 
 function adjustTime() {
@@ -27,7 +42,7 @@ function adjustTime() {
 }
 
 $(document).ready(function() {
-	changeBg();
+	updateImage();
 	adjustTime();
 
 	$("#title").click(function() {
@@ -45,6 +60,25 @@ $(document).ready(function() {
 		var timeOpacity = Math.abs(12 - new Date().getHours())*0.1 + (new Date().getMinutes()/60.0)*0.01
 		$("#night").fadeTo(8000, timeOpacity);
 	});
+
+	$("#demo").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#day").offset().top
+		}, 400);
+		$("#night").fadeTo(1000, 0);
+		$("#night").fadeTo(8000, 1);
+		$("#night").fadeTo(8000, 0);
+		var timeOpacity = Math.abs(12 - new Date().getHours())*0.1 + (new Date().getMinutes()/60.0)*0.01
+		$("#night").fadeTo(1000, timeOpacity);
+	});
+
+	$("#next").click(function() {
+		var previous = index;
+		while (index === previous) {
+			index = Math.floor(Math.random() * images.length);
+		}
+	 	updateImage();
+	})
 
 	setInterval(adjustTime, 1000*60);
 });
